bd0ced233caf7d539dd23989cc8f35f194a99531
Thom Snoeren <28123215+TSN-ADMIN@users.noreply.github.com>
2019-06-27 07:29:14 +0200

    Add files via upload

---
+++ dummy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import (
+    division,
+    absolute_import,
+    print_function,
+    unicode_literals
+)
+
+"""Main SPyE module.
+
+#TODO, module docstring
+
+Scintilla Python based Editor
+"""
+
